3754282c1ffe4a72cb9277774ef3e512bbe7ddb1
Parajulibkrm <39263700+Parajulibkrm@users.noreply.github.com>
2018-12-10 21:13:45 +0545

    Update calculator.js
    Okay

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ var exp = 0;
 var result;
 
 document.getElementById("calc-operation").innerHTML = exp;
+
 function insert(a) {
 	a = a.toString();
 	// if (a = "0" || a = "1" || a = "2" || a = "3" || a = "4" || a = "5" || a = "6" || a = "7" || a = "8" || a = "9" ) {
@@ -27,10 +28,19 @@ function insert(a) {
 	}
 	document.getElementById("calc-operation").innerHTML = exp;
 }
+
 function equals() {
 	exp = document.getElementById("calc-operation").innerHTML;
 	if (exp) {
 		result = eval(exp);
+	}
+
+	if (result.toString().length > 9) {
+		result = short(result);
+		result = eval(result);
+	}
+	if (result.toString().length > 10) {
+		result = expo(result);
 	}
 	document.getElementById("calc-typed").innerHTML = result;
 
@@ -53,5 +63,20 @@ function backspace() {
 }
 
 function short(ans) {
-numObj.toFixed(6);
+	var ans1 = ans;
+	abc = 9 - ans1.toFixed(0).length;
+	if (abc >= 0) {
+		return ans.toFixed(abc);
+	}
+	else {
+		return ans.toFixed(0);
+	}
+}
+
+function expo(result) {
+	var power = result.toString().length - 1;
+	console.log(result/(Math.pow(10, power)));
+	answ = result/(Math.pow(10, power))
+	answe = answ.toFixed(3) + 'x10' + power.toString().sup();
+	return answe;
 }
